bd0503ca2c1d0d58a3d0c8a904e43e2625ec50bc
rodrigoeduardo <rodrigoow99@hotmail.com>
2022-04-13 12:32:19 -0300

    now guessed player info is adaptable

--- src/components/GuessesBox/index.tsx
+++ src/components/GuessesBox/index.tsx
@@ -1,29 +1,32 @@
-import { Flex, Text, VStack } from '@chakra-ui/react';
-import { useContext } from 'react';
-import { GameContext } from '../../contexts/GameContext';
-import { InfoBox } from './InfoBox';
+import { Flex, Text, VStack } from "@chakra-ui/react";
+import { useContext } from "react";
+import { GameContext } from "../../contexts/GameContext";
+import { Player } from "../../types/Player";
+import { InfoBox } from "./InfoBox";
 
 export function GuessesBox() {
-  const { correctPlayer } = useContext(GameContext);
+  const { guesses } = useContext(GameContext);
 
   return (
     <VStack>
-      <Flex direction="column">
-        <Text
-          textAlign="center"
-          fontWeight="bold"
-          fontSize={22}
-          letterSpacing="0.1rem"
-          color="purple.500"
-        >
-          FALLEN
-        </Text>
-        <Flex direction="column" textAlign="center">
-          <InfoBox info="Denmark" type="nationality" />
-          <InfoBox info="In-game leader" type="role" />
-          <InfoBox info="$1,832,900" type="winnings" />
+      {guesses.map((guess: Player) => (
+        <Flex key={guess.id} direction="column">
+          <Text
+            textAlign="center"
+            fontWeight="bold"
+            fontSize={22}
+            letterSpacing="0.1rem"
+            color="purple.500"
+          >
+            {guess.name}
+          </Text>
+          <Flex direction="column" textAlign="center">
+            <InfoBox info={guess.nationality} type="nationality" />
+            <InfoBox info={guess.role} type="role" />
+            <InfoBox info={guess.totalWinnings} type="winnings" />
+          </Flex>
         </Flex>
-      </Flex>
+      ))}
     </VStack>
   );
 }
