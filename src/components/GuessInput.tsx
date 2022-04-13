5928b1d4973b2891e7884ce0660ccaa10f8439c1
rodrigoeduardo <rodrigoow99@hotmail.com>
2022-04-13 12:32:54 -0300

    now it's possible to guess a player

--- src/components/GuessInput.tsx
+++ src/components/GuessInput.tsx
@@ -1,98 +1,102 @@
-import { Box, Flex, Input, Text } from '@chakra-ui/react';
-import { FormEvent, useRef, useState } from 'react';
-import { Player } from '../types/Player';
+import { Box, Flex, Input, Text } from "@chakra-ui/react";
+import { useContext, useState } from "react";
+import { Controller, useForm } from "react-hook-form";
+import { GameContext } from "../contexts/GameContext";
+import { Player } from "../types/Player";
 
 export function GuessInput() {
   // temporary players array
   const players: Player[] = [
     {
       id: 0,
-      name: 'device',
-      link: 'https://liquipedia.net/counterstrike/Device',
-      nationality: 'Denmark',
+      name: "device",
+      link: "https://liquipedia.net/counterstrike/Device",
+      nationality: "Denmark",
       photoURL:
-        'https://liquipedia.net/commons/images/thumb/4/46/Dev1ce_%40_PGL_Major_Stockholm_2021.jpg/600px-Dev1ce_%40_PGL_Major_Stockholm_2021.jpg',
-      role: 'AWPer',
-      totalWinnings: '$1,949,050',
+        "https://liquipedia.net/commons/images/thumb/4/46/Dev1ce_%40_PGL_Major_Stockholm_2021.jpg/600px-Dev1ce_%40_PGL_Major_Stockholm_2021.jpg",
+      role: "AWPer",
+      totalWinnings: "$1,949,050",
     },
     {
       id: 1,
-      name: 'dupreeh',
-      link: 'https://liquipedia.net/counterstrike/Dupreeh',
-      nationality: 'Denmark',
+      name: "dupreeh",
+      link: "https://liquipedia.net/counterstrike/Dupreeh",
+      nationality: "Denmark",
       photoURL:
-        'https://liquipedia.net/commons/images/thumb/4/4d/Dupreeh_%40_PGL_Major_Stockholm_2021.jpg/600px-Dupreeh_%40_PGL_Major_Stockholm_2021.jpg',
-      role: 'Rifler',
-      totalWinnings: '$1,970,873',
+        "https://liquipedia.net/commons/images/thumb/4/4d/Dupreeh_%40_PGL_Major_Stockholm_2021.jpg/600px-Dupreeh_%40_PGL_Major_Stockholm_2021.jpg",
+      role: "Rifler",
+      totalWinnings: "$1,970,873",
     },
     {
       id: 61,
-      name: 'dennis',
-      link: 'https://liquipedia.net/counterstrike/Dennis',
-      nationality: 'Sweden',
+      name: "dennis",
+      link: "https://liquipedia.net/counterstrike/Dennis",
+      nationality: "Sweden",
       photoURL:
-        'https://liquipedia.net/commons/images/thumb/7/73/Dennis_%40_EPICENTER_2018.jpg/600px-Dennis_%40_EPICENTER_2018.jpg',
-      role: 'In-game leader',
-      totalWinnings: '$364,125',
+        "https://liquipedia.net/commons/images/thumb/7/73/Dennis_%40_EPICENTER_2018.jpg/600px-Dennis_%40_EPICENTER_2018.jpg",
+      role: "In-game leader",
+      totalWinnings: "$364,125",
     },
     {
       id: 62,
-      name: 'KRIMZ',
-      link: 'https://liquipedia.net/counterstrike/KRIMZ',
-      nationality: 'Sweden',
+      name: "KRIMZ",
+      link: "https://liquipedia.net/counterstrike/KRIMZ",
+      nationality: "Sweden",
       photoURL:
-        'https://liquipedia.net/commons/images/thumb/b/b4/KRIMZ_%40_IEM_Katowice_2022.jpg/600px-KRIMZ_%40_IEM_Katowice_2022.jpg',
-      role: 'Rifler',
-      totalWinnings: '$920,551',
+        "https://liquipedia.net/commons/images/thumb/b/b4/KRIMZ_%40_IEM_Katowice_2022.jpg/600px-KRIMZ_%40_IEM_Katowice_2022.jpg",
+      role: "Rifler",
+      totalWinnings: "$920,551",
     },
     {
       id: 63,
-      name: 'Maikelele',
-      link: 'https://liquipedia.net/counterstrike/Maikelele',
-      nationality: 'Sweden',
+      name: "Maikelele",
+      link: "https://liquipedia.net/counterstrike/Maikelele",
+      nationality: "Sweden",
       photoURL:
-        'https://liquipedia.net/commons/images/thumb/6/68/Maikelele_DreamHack_Open_Sevilla_2019.jpg/600px-Maikelele_DreamHack_Open_Sevilla_2019.jpg',
-      role: 'AWPer',
-      totalWinnings: '$131,649',
+        "https://liquipedia.net/commons/images/thumb/6/68/Maikelele_DreamHack_Open_Sevilla_2019.jpg/600px-Maikelele_DreamHack_Open_Sevilla_2019.jpg",
+      role: "AWPer",
+      totalWinnings: "$131,649",
     },
     {
       id: 311,
-      name: 'boltz',
-      link: 'https://liquipedia.net/counterstrike/Boltz',
-      nationality: 'Brazil',
+      name: "boltz",
+      link: "https://liquipedia.net/counterstrike/Boltz",
+      nationality: "Brazil",
       photoURL:
-        'https://liquipedia.net/commons/images/thumb/6/68/Boltz_%40_IEM_Cologne_2021.jpg/600px-Boltz_%40_IEM_Cologne_2021.jpg',
-      role: 'Rifler',
-      totalWinnings: '$382,922',
+        "https://liquipedia.net/commons/images/thumb/6/68/Boltz_%40_IEM_Cologne_2021.jpg/600px-Boltz_%40_IEM_Cologne_2021.jpg",
+      role: "Rifler",
+      totalWinnings: "$382,922",
     },
     {
       id: 312,
-      name: 'FalleN',
-      link: 'https://liquipedia.net/counterstrike/FalleN',
-      nationality: 'Brazil',
+      name: "FalleN",
+      link: "https://liquipedia.net/counterstrike/FalleN",
+      nationality: "Brazil",
       photoURL:
-        'https://liquipedia.net/commons/images/thumb/7/7b/FalleN_%40_PGL_Major_Stockholm_2021.jpg/600px-FalleN_%40_PGL_Major_Stockholm_2021.jpg',
-      role: 'In-game leader',
-      totalWinnings: '$1,125,229',
+        "https://liquipedia.net/commons/images/thumb/7/7b/FalleN_%40_PGL_Major_Stockholm_2021.jpg/600px-FalleN_%40_PGL_Major_Stockholm_2021.jpg",
+      role: "In-game leader",
+      totalWinnings: "$1,125,229",
     },
     {
       id: 313,
-      name: 'fer',
-      link: 'https://liquipedia.net/counterstrike/Fer',
-      nationality: 'Brazil',
+      name: "fer",
+      link: "https://liquipedia.net/counterstrike/Fer",
+      nationality: "Brazil",
       photoURL:
-        'https://liquipedia.net/commons/images/thumb/5/5e/Fer_%40_EPL_S10_Finals.jpg/600px-Fer_%40_EPL_S10_Finals.jpg',
-      role: 'Rifler',
-      totalWinnings: '$1,045,102',
+        "https://liquipedia.net/commons/images/thumb/5/5e/Fer_%40_EPL_S10_Finals.jpg/600px-Fer_%40_EPL_S10_Finals.jpg",
+      role: "Rifler",
+      totalWinnings: "$1,045,102",
     },
   ];
 
-  const newGuess = useRef<HTMLInputElement>(null);
+  const { guesses, setGuesses } = useContext(GameContext);
+
+  const { control, handleSubmit, setValue, setFocus } = useForm();
 
   const [filteredPlayers, setFilteredPlayers] = useState<Player[]>();
 
   const handleSearch = (userInput: string) => {
-    if (userInput.length === 0) {
+    if (userInput.length <= 1) {
       setFilteredPlayers(undefined);
       return;
     }
@@ -104,39 +108,64 @@ export function GuessInput() {
     setFilteredPlayers(filtered);
   };
 
-  const handleSubmit = (event: FormEvent) => {
-    event.preventDefault();
-    console.log(newGuess.current?.value);
+  const onSubmit = (data: any) => {
+    // get player in players
+    const playerGuessed = players.filter((player) =>
+      player.name.toLowerCase().includes(data.guess.toLowerCase())
+    )[0];
+
+    if (playerGuessed.name.toLowerCase() === data.guess.toLowerCase()) {
+      const newGuesses = [playerGuessed, ...guesses];
+      setGuesses(newGuesses);
+      setValue("guess", "");
+      setFilteredPlayers(undefined);
+    }
   };
 
-  const handleAutoCompleteClick = (player: string) => {
-    // set the value of the input to the selected player
-    console.log(player);
+  const handleAutoCompleteClick = (playerName: string) => {
+    setValue("guess", playerName);
+    setFilteredPlayers(undefined);
+    setFocus("guess");
   };
 
   return (
     <Flex direction="column" align="center" width="100%" my={8}>
-      <Box as="form" onSubmit={handleSubmit} width="100%" align="center">
-        <Input
-          onChange={(e) => handleSearch(e.target.value)}
-          maxW="360px"
-          width="80%"
-          variant="unstyled"
-          color="black"
-          placeholder="Enter your guess"
-          _placeholder={{
-            color: 'gray.500',
-          }}
-          fontWeight="medium"
-          fontSize="lg"
-          bgColor="purple.50"
-          height={16}
-          px={4}
-          transition="all 0.2s"
-          _focus={{
-            border: '1px solid white',
-          }}
-          ref={newGuess}
+      <Box
+        as="form"
+        onSubmit={handleSubmit(onSubmit)}
+        width="100%"
+        align="center"
+      >
+        <Controller
+          name="guess"
+          control={control}
+          render={({ field: { onChange, value, ref } }) => (
+            <Input
+              ref={ref}
+              value={value || ""}
+              onChange={(e: any) => {
+                handleSearch(e.target.value);
+                onChange(e);
+              }}
+              maxW="360px"
+              width="80%"
+              variant="unstyled"
+              color="black"
+              placeholder="Enter your guess"
+              _placeholder={{
+                color: "gray.500",
+              }}
+              fontWeight="medium"
+              fontSize="lg"
+              bgColor="purple.50"
+              height={16}
+              px={4}
+              transition="all 0.2s"
+              _focus={{
+                border: "1px solid white",
+              }}
+            />
+          )}
         />
       </Box>
       {!!filteredPlayers && (
@@ -154,8 +183,8 @@ export function GuessInput() {
               borderRadius={10}
               fontSize={20}
               _hover={{
-                cursor: 'pointer',
-                bgColor: 'purple.100',
+                cursor: "pointer",
+                bgColor: "purple.100",
               }}
               px={2}
               py={3}
