962aeb2d4fa61d3c2cf0745aedc73faa14bba918
Rodrigo Eduardo <rodrigoow99@hotmail.com>
2022-04-14 01:23:45 -0300

    spacing between guessed players increased

--- src/components/GuessesBox/index.tsx
+++ src/components/GuessesBox/index.tsx
@@ -1,14 +1,14 @@
-import { Flex, Text, VStack } from "@chakra-ui/react";
-import { useContext } from "react";
-import { GameContext } from "../../contexts/GameContext";
-import { Player } from "../../types/Player";
-import { InfoBox } from "./InfoBox";
+import { Flex, Text, VStack } from '@chakra-ui/react';
+import { useContext } from 'react';
+import { GameContext } from '../../contexts/GameContext';
+import { Player } from '../../types/Player';
+import { InfoBox } from './InfoBox';
 
 export function GuessesBox() {
   const { guesses } = useContext(GameContext);
 
   return (
-    <VStack>
+    <VStack spacing={6}>
       {guesses.map((guess: Player) => (
         <Flex key={guess.id} direction="column">
           <Text
@@ -16,11 +16,11 @@ export function GuessesBox() {
             fontWeight="bold"
             fontSize={22}
             letterSpacing="0.1rem"
-            color="purple.500"
+            color="gray.50"
           >
-            {guess.name}
+            {guess.name.toUpperCase()}
           </Text>
-          <Flex direction="column" textAlign="center">
+          <Flex direction="column" textAlign="center" mt={1}>
             <InfoBox info={guess.nationality} type="nationality" />
             <InfoBox info={guess.role} type="role" />
             <InfoBox info={guess.totalWinnings} type="winnings" />
