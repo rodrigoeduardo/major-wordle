8fa111f6e8c126b8572ed4760ff25dd2a810e267
Rodrigo Eduardo <rodrigoow99@hotmail.com>
2022-04-14 01:22:39 -0300

    at every new guess it's checked if it's correct

--- src/contexts/GameContext.tsx
+++ src/contexts/GameContext.tsx
@@ -1,10 +1,11 @@
-import React, { createContext, useState } from "react";
-import { Player } from "../types/Player";
+import React, { createContext, useEffect, useState } from 'react';
+import { Player } from '../types/Player';
 
 interface GameContextData {
   correctPlayer: Player;
   guesses: Player[];
   setGuesses: (newGuesses: Player[]) => void;
+  hasWon: boolean;
 }
 
 export const GameContext = createContext({} as GameContextData);
@@ -15,21 +16,34 @@ export function GameContextProvider({
   children: React.ReactNode;
 }) {
   const [guesses, setGuesses] = useState<Player[]>([]);
+  const [hasWon, setHasWon] = useState(false);
 
   // temporary static correct player
   const correctPlayer = {
     id: 312,
-    name: "FalleN",
-    link: "https://liquipedia.net/counterstrike/FalleN",
-    nationality: "Brazil",
+    name: 'FalleN',
+    link: 'https://liquipedia.net/counterstrike/FalleN',
+    nationality: 'Brazil',
     photoURL:
-      "https://liquipedia.net/commons/images/thumb/7/7b/FalleN_%40_PGL_Major_Stockholm_2021.jpg/600px-FalleN_%40_PGL_Major_Stockholm_2021.jpg",
-    role: "In-game leader",
-    totalWinnings: "$1,125,229",
+      'https://liquipedia.net/commons/images/thumb/7/7b/FalleN_%40_PGL_Major_Stockholm_2021.jpg/600px-FalleN_%40_PGL_Major_Stockholm_2021.jpg',
+    role: 'In-game leader',
+    totalWinnings: '$1,125,229',
   };
 
+  // check if last guess is correct
+  useEffect(() => {
+    if (guesses.length > 0) {
+      const lastGuess = guesses[0];
+      if (lastGuess.name === correctPlayer.name) {
+        setHasWon(true);
+      }
+    }
+  }, [guesses]);
+
   return (
-    <GameContext.Provider value={{ correctPlayer, guesses, setGuesses }}>
+    <GameContext.Provider
+      value={{ correctPlayer, guesses, setGuesses, hasWon }}
+    >
       {children}
     </GameContext.Provider>
   );
